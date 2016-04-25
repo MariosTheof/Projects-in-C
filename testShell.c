9f1b3ec133ae6fae520d4654def026345d5a2fbe
Marios Athanasiadis <Athmarios@gmail.com>
2016-04-25 13:59:26 +0300

    First attempt to implement myshell with functions.

--- testShell.c
+++ testShell.c
@@ -5,41 +5,53 @@
 #include<sys/types.h> /* for pid_t */
 #include<sys/wait.h> /* fpr wait() */
 
+/*Reads a line from the terminal */
+void readLine(void)
+{
+
+	if (fgets(fileName,sizeof(fileName), stdin) != 0){
+
+		fileName[strcspn(fileName, "\n")] = '\0'; //strcspn = string span ?
+
+			char *args[129];
+			char **argv = args;
+			char *cmd = fileName;
+			const char *whisp = " \t\f\r\b\n";
+			char *token;
+
+			while ((token = strtok(cmd,whisp)) != 0) // strtok = breaks string into a series of tokens using the delimeter delim(whisp)
+			{
+				*argv++ = token;
+				cmd = 0;
+			}// /while
+
+			if (feof(stdin){
+			break;
+			}
+
+			*argv = 0;
+	}
+
+}
+
+
+
 int main(int argc, char* argv[])
-{	
+{
 	pid_t pid,waitPid;
 	int status;
 	char fileName[256];
-	while(!feof(stdin))
+
+	while(1)
 	{
+
 	printf(" %s > ", argv[0]);
-	if (fgets(fileName, sizeof(fileName), stdin) != 0)
-	{
-		fileName[strcspn(fileName, "\n")] = '\0'; //strcspn = calculates the length of the initial fileName segment, which  consists of chars not in str2("\n")
-	
-		char *args[129];
-		char **argv = args;
-		char *cmd = fileName;
-		const char *whisp = " \t\f\r\b\n";
-		char *token;
-		while ((token = strtok(cmd,whisp)) != 0) // strtok = breaks string into a series of tokens using the delimeter delim(whisp)
-		{
-			*argv++ = token;
-			cmd = 0;
-		}// /while
 
-		if (feof(stdin))
-		{
-		break;
-		}
-
-
-		*argv = 0;
-		
+  readLine();
 
 		pid = fork();
 		if (pid == 0){
-		
+
 		/*execlp goes first to run a shell command if there is one.
 		If not, then execv will execute a file of our own. */
 		execlp(args[0],args[0], args[1],NULL);
@@ -48,12 +60,11 @@ int main(int argc, char* argv[])
 
 		fprintf(stderr, "Oops! \n");
 		break;
+
 		} // /if
 		waitPid = wait(&status);
 
-		
 
-	}// /if(fgets..)
 	} // /while
 	return 1;
 }
