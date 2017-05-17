efeafec945972bdc0455a2c410797f36f494217a
MariosTheof <MariosTheof@users.noreply.github.com>
2017-05-17 03:24:38 +0300

    Added a randomly populated array

--- Ide.c
+++ Ide.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 int n,s,numOfthreads;
 
 int main(void) {
-	// your code goes here
+	
+	
 	
 	printf("Enter size of array: \n");
 	 //input_value
@@ -18,7 +20,20 @@ int main(void) {
 	
 	scanf("%d", &numOfthreads);
 	
-	printf("Size of array : %d , seed : %d , number of threads : %d ", n, s, numOfthreads);
+	printf("Size of array : %d , seed : %d , number of threads : %d \n", n, s, numOfthreads);
+	
+	int initialArray[n],i; // initial array that is going to be merge sorted
+	
+	srand(s); // Seeds the pseudo-random number generator
+	
+	// Fills the array with random generated numbers
+	for (i = 0; i < n; i++ ){
+	    initialArray[i] = rand();
+	    printf("[%d] : %d \n", i+1, initialArray[i]);
+	}
+	
+	
+	
+
 	return 0;
 }
-
